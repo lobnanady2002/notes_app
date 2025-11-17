a4969f61b9091439460a2879ad22f4b93c769974
lobnanady2002 <lobnanady20022@gmail.com>
2025-11-17 14:15:01 +0200

    create notes cubit

--- lib/widgets/notes_list_view.dart
+++ lib/widgets/notes_list_view.dart
@@ -2,8 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:notes_app/widgets/custom_note_item.dart';
 class NotesListView extends StatelessWidget {
   const NotesListView({super.key});
-
-
   @override
   Widget build(BuildContext context) {
     return Padding(
